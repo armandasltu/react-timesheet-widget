c4e4d8c092684496910c4f44e067cb8da2d97053
Armandas Motuzas <armandasmotuzas@Armandass-MBP.lan>
2020-02-02 23:44:50 +0200

    Better mocked data

--- src/actions/events/mocks/index.js
+++ src/actions/events/mocks/index.js
@@ -13,11 +13,13 @@ const generateEvents = (count = 50) => {
     const date = moment().subtract(getRandomNumber(0, 7), 'days');
     const firstTaskStart = moment()
       .subtract(day, 'days')
-      .subtract(getRandomNumber(0, 7), 'hours')
+      .subtract(getRandomNumber(7, 11), 'hours')
       .subtract(getRandomNumber(0, 60), 'minutes')
       .toDate();
     const lastTaskEnd = moment()
       .subtract(day, 'days')
+      .subtract(getRandomNumber(1, 6), 'hours')
+      .subtract(getRandomNumber(0, 60), 'minutes')
       .toDate();
     const quantity = getRandomNumber(1, 10);
     const price = getRandomNumber(0, 10) + '.' + getRandomNumber(0, 99);
