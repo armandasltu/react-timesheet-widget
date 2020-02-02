6c1aa3c960b56c71ed8d19b5cd262e373a0cb635
Armandas Motuzas <armandasmotuzas@Armandass-MBP.lan>
2020-02-02 18:49:52 +0200

    Better styling

--- src/App.js
+++ src/App.js
@@ -2,16 +2,13 @@ import React, { PureComponent } from 'react';
 import { StoreProvider } from './store';
 import reducers from './reducers';
 import initialState from './store/initialState';
-import './assets/sass/main.scss';
 import TimesheetWidget from './containers/TimesheetWidget';
 
 class App extends PureComponent {
   render() {
     return (
       <StoreProvider initialState={initialState} reducer={reducers}>
-        <div className="container">
-          <TimesheetWidget />
-        </div>
+        <TimesheetWidget />
       </StoreProvider>
     );
   }
